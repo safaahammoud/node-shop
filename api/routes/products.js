dc5459a6f739ef7de79fefdeb707f6397dab1ce0
Safaa <hammoudsafaa@gmail.com>
2025-01-06 23:46:52 +0200

    refactor(products-api-routes): folder structure

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('./models/product');
+const Product = require('../models/product');
 const mongoose = require('mongoose');
-const product = require('./models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
