c9dcdcbcaac23922d3be0ea0efb5b8e26bc6a478
Safaa <hammoudsafaa@gmail.com>
2025-01-16 22:44:09 +0200

    feat(user): add login,signup and image upload

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect(`mongodb+srv://SH:${process.env.MONGO_ATLAS_PW}@node-shop.r5tds.mongodb.net/`);
 
@@ -23,8 +24,10 @@ app.use((req, res, next) => {
 });
 
 // routes that handle requests
+app.use('/uploads', express.static('uploads'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req,res,next)=>{
     const error = new Error('not found');
